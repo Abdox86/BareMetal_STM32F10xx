2a1c2e2c950d07a40fa4728805a35d9e148d7896
abdulfatah <abdulfatahalturshani98@gmail.com>
2022-02-08 22:45:35 +0200

    assembly in C test

--- STM32F10xx_Driver.h
+++ STM32F10xx_Driver.h
@@ -1,6 +1,7 @@
 #ifndef STM32F10xx_Driver_H
 #define STM32F10xx_Driver_H
 
+/*empty prototype*/
 void make();
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,30 @@
-#include "STM32F10xx_Driver.h"
+
+
 int main() {
+    
+  __asm__(
+      "rcc    = 0x40021018    \n"
+      "trisa  = 0x40010800    \n"
+      "porta  = 0x4001080c    \n"
+
+      "mov r1 , #0x4        \n"
+      "ldr r2 , =rcc        \n"
+      "str r1 , [r2]        \n"
+      
+      "mov r1 , #0x2        \n"
+      "ldr r2 , =trisa      \n"
+      "str r1 , [r2]        \n"
+      
+      "ldr r2 , =porta      \n"
+
+      "BLINK:               \n"
+      "mov r1 , #0x0        \n"
+      "str r1 , [r2]        \n"
+      "mov r1 , #0x1        \n"
+      "str r1 , [r2]        \n"
+      "b   BLINK            \n"
+  );  
+
 }
 
 
